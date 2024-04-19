1bc81158de9a824c992943d024d93b0645860f6a
Mohamed Gouda <goda61@gmail.com>
2024-04-20 00:14:56 +0200

    Add templates files

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # My Apps :
+    'expenses',
 ]
 
 MIDDLEWARE = [
@@ -116,9 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/ataticfiles')]
+# STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/ataticfiles')]
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 # Default primary key field type
--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from expenses.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('',index),
 
 ]
