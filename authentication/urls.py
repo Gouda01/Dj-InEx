f9b09789575b5204825366b8c31022d766543fa1
Mohamed Gouda <goda61@gmail.com>
2024-04-20 23:56:45 +0200

    Add pard of username validation in register

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
-from .views import RegistrationView
+from . import views
 
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name="register")
+    path('register/', views.RegistrationView.as_view(), name="register"),
+    path('validate-username',csrf_exempt(views.UsernameValidationView.as_view()), name='validate-username')
 ]
