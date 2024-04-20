453b1a216f4ed01e0bc50f506ece80ead3646ce3
Mohamed Gouda <goda61@gmail.com>
2024-04-21 00:36:34 +0200

    Add pard of username validation in register -

--- static/custom/register.js
+++ static/custom/register.js
@@ -5,6 +5,9 @@ usernameField.addEventListener("keyup", (e) => {
     console.log('777777',7777777);
     const usernameVal = e.target.value;
 
+    usernameField.classList.remove("is-invalid");
+    feedbackArea.computedStyleMap.display="none";
+
     if(usernameVal.length>0){
         fetch("/authentication/validate-username", {
         body: JSON.stringify({username:usernameVal}),
@@ -16,7 +19,7 @@ usernameField.addEventListener("keyup", (e) => {
         if (data.username_error){
             usernameField.classList.add("is-invalid");
             feedbackArea.computedStyleMap.display="block";
-            feedbackArea.innerHTML='<p>${data.username_error}</p>';
+            feedbackArea.innerHTML=`<p>${data.username_error}</p>`;
         }
     });
     }
