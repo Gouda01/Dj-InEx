c7ee14467d7618215c589f315d400af737d98cc3
Mohamed Gouda <goda61@gmail.com>
2024-04-21 16:36:29 +0200

    Add validation to username and email in register page

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegistrationView.as_view(), name="register"),
-    path('validate-username',csrf_exempt(views.UsernameValidationView.as_view()), name='validate-username')
+    path('validate-username',csrf_exempt(views.UsernameValidationView.as_view()), name='validate-username'),
+    path('validate-email',csrf_exempt(views.EmailValidationView.as_view()), name='validate-email'),
 ]
--- static/custom/register.js
+++ static/custom/register.js
@@ -1,9 +1,39 @@
 const usernameField=document.querySelector('#usernameField')
-const feedbackArea=document.querySelector('.invalid-feedback')
+const feedbackArea=document.querySelector('.invalid_feedback')
+
+const emailField=document.querySelector('#emailField')
+const emailFeedbackArea=document.querySelector('.emailFeedbackArea')
+
+const usernameSuccessOutput=document.querySelector('.usernameSuccessOutput')
+
+
+emailField.addEventListener("keyup", (e) => {
+    const emailVal = e.target.value;
+
+    emailField.classList.remove("is-invalid");
+    emailFeedbackArea.computedStyleMap.display="none";
+
+    if(emailVal.length>0){
+        fetch("/authentication/validate-email", {
+        body: JSON.stringify({email:emailVal}),
+        method:"POST",
+    })
+    .then(res=>res.json())
+    .then((data) => {
+        if (data.email_error){
+            emailField.classList.add("is-invalid");
+            emailFeedbackArea.computedStyleMap.display="block";
+            emailFeedbackArea.innerHTML=`<p>${data.email_error}</p>`;
+        }
+    });
+    }
+});
 
 usernameField.addEventListener("keyup", (e) => {
-    console.log('777777',7777777);
     const usernameVal = e.target.value;
+
+    usernameSuccessOutput.style.display = "block";
+    usernameSuccessOutput.textContent = `Checking ${usernameVal}`;
 
     usernameField.classList.remove("is-invalid");
     feedbackArea.computedStyleMap.display="none";
@@ -15,7 +45,7 @@ usernameField.addEventListener("keyup", (e) => {
     })
     .then(res=>res.json())
     .then((data) => {
-        console.log("data",data)
+        usernameSuccessOutput.style.display = "none";
         if (data.username_error){
             usernameField.classList.add("is-invalid");
             feedbackArea.computedStyleMap.display="block";
