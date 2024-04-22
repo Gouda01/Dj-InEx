8c84b555ee00cd8697978eec9a70418ff5d1cdd2
Mohamed Gouda <goda61@gmail.com>
2024-04-22 16:24:39 +0200

    Add send activate link to email

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', views.RegistrationView.as_view(), name="register"),
     path('validate-username',csrf_exempt(views.UsernameValidationView.as_view()), name='validate-username'),
     path('validate-email',csrf_exempt(views.EmailValidationView.as_view()), name='validate-email'),
+    path('activate/<uidb64>/<token>',csrf_exempt(views.verificationView.as_view()), name='activate'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 import json
 from django.http import JsonResponse
@@ -6,6 +6,13 @@ from django.contrib.auth.models import User
 from validate_email import validate_email
 from django.contrib import messages
 from django.core.mail import send_mail
+
+from django.utils.encoding import force_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+
+from .utils import token_generator
 
 
 # Create your views here.
@@ -68,10 +75,15 @@ class RegistrationView(View):
                 user.set_password(password1)
                 user.set_active = False
                 user.save()
+                uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+                
+                domain = get_current_site(request).domain
+                link = reverse('activate', kwargs={'uidb64':uidb64, 'token': token_generator.make_token(user)})
+                activate_url = 'http://'+ domain + link
 
                 #send email
                 email_subject = 'Activate your account'
-                email_body = 'Test Body'
+                email_body = 'Hi '+ user.username + ' Please use this link to verify your account\n' + activate_url
 
                 send_mail(
                     email_subject,
@@ -85,3 +97,7 @@ class RegistrationView(View):
                 return render (request,'authentication/register.html')
             
         return render (request,'authentication/register.html')
+    
+class verificationView(View):
+    def get(self, request, uidb64, token):
+        return redirect('login')
