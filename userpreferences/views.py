1d8c4d38ed1af53ec698f99df8f742facf10388e
Mohamed Gouda <goda61@gmail.com>
2024-04-23 23:27:01 +0200

    Add Currency in view and templates

--- userpreferences/views.py
+++ userpreferences/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 import os
 import json
 from django.conf import settings
+from django.contrib import messages
+
+from .models import UserPreferences
 
 
 # Create your views here.
@@ -9,9 +12,33 @@ def index (request):
 
     currency_data = []
     file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
-
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
         for k, v in data.items():
             currency_data.append({'name': k, 'value': v})
-    return render(request, 'preferences/index.html', {'currencies':currency_data})
+
+    user_perferences = None
+    exists = UserPreferences.objects.filter(user=request.user)
+    if exists :
+        user_perferences = UserPreferences.objects.get(user=request.user)
+
+
+    if request.method == 'POST' :
+
+        currency = request.POST['currency']
+
+        if exists :
+            user_perferences.currency = currency
+            user_perferences.save()
+            messages.success(request, 'Changes saved')
+
+        else :
+            UserPreferences.objects.create(
+                user = request.user,
+                currency = currency
+            )
+
+        messages.success(request, 'Changes saved')
+        return render(request, 'preferences/index.html', {'currencies':currency_data,'user_perferences':user_perferences})
+    
+    return render(request, 'preferences/index.html', {'currencies':currency_data,'user_perferences':user_perferences})
