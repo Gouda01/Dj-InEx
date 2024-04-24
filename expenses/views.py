410af72d6d048fd722d857e469be32b1dc9bf32e
Mohamed Gouda <goda61@gmail.com>
2024-04-24 23:57:32 +0200

    Add add expence template page and some view

--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import Expense, Category
 
@@ -18,5 +19,42 @@ def add_expense (request):
     categories = Category.objects.all()
     context = {
         'categories':categories,
+        'values' : request.POST
     }
+
+    if request.method =='POST' :
+        amount = request.POST['amount']
+        description = request.POST['description']
+        category = request.POST['category']
+        date = request.POST['date']
+
+        if not amount :
+            messages.error(request, 'Amount Required')
+            return render(request,'expenses/add_expense.html',context)
+        
+        if not description :
+            messages.error(request, 'Description Required')
+            return render(request,'expenses/add_expense.html',context)
+        
+        if not category :
+            messages.error(request, 'Category Required')
+            return render(request,'expenses/add_expense.html',context)
+        
+        if not date :
+            messages.error(request, 'Expense date Required')
+            return render(request,'expenses/add_expense.html',context)
+        
+        Expense.objects.create(
+            amount = amount,
+            description = description,
+            category = category,
+            date = date,
+        )
+
+        messages.success(request, 'Expense Saved')
+        return render(request,'expenses/add_expense.html',context)
+
+    
     return render(request,'expenses/add_expense.html',context)
+
+    
