081e03cc07abd8f8e99fe049dd6d4462cccca38c
Mohamed Gouda <goda61@gmail.com>
2024-04-24 23:15:51 +0200

    Add add expence template page

--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Expense, Category
+
 
 # Create your views here.
 
@@ -13,4 +15,8 @@ def index (request):
 @login_required(login_url='/authentication/login/')
 def add_expense (request):
 
-    return render(request,'expenses/add_expense.html',{})
+    categories = Category.objects.all()
+    context = {
+        'categories':categories,
+    }
+    return render(request,'expenses/add_expense.html',context)
